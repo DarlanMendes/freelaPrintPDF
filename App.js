810a0445b3be16b7982ad52e7af46dea9d7e0fa3
Darlan Mendes <dedsondarlan@gmail.com>
2023-01-27 13:48:23 -0300

    Adicao do axios

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { printToFileAsync } from 'expo-print';
 import { shareAsync } from 'expo-sharing';
 export default function App() {
@@ -14,11 +14,20 @@ export default function App() {
     { produto: 'Cerveja Heineken 600ml', qtd: 1, precoUnit: 18 },
     { produto: 'Cerveja Heineken 600ml', qtd: 1, precoUnit: 18 }
   ])
+  useEffect(()=>{
+    const DataHoje = new Date()
+    setData(`${DataHoje.getDay()<10?'0'+DataHoje.getDay():DataHoje.getDay()}`+'/'+`${DataHoje.getMonth()+1<10?"0"+eval(DataHoje.getMonth()+1):DataHoje.getMonth()+1}`+'/'+DataHoje.getFullYear());
+    setHora(DataHoje.getHours()+':'+DataHoje.getMinutes()+':'+DataHoje.getSeconds())
+  })
+ 
   let totalNota=0;
+  let totalItens=0;
   pedido.forEach((item)=>{
-    totalNota = item.precoUnit + totalNota
+    totalNota = item.precoUnit + totalNota;
+    totalItens = item.qtd + totalItens;
    })
-
+   
+  
   async function gerarPDF() {
     const file = await printToFileAsync({
       html: html,
@@ -27,34 +36,45 @@ export default function App() {
     await shareAsync(file.uri)
   }
 
+  
   const html = `
   <html>
-    <body style="display:flex;justify-content:center;flex-direction:column;margin:0px;">
+    <body style="display:flex;flex:1;flex-direction:column;margin:0px;height:100%;width:100vw">
         
-        <h1 style="text-align:center;font-size:75px;background-color:#ccc;margin:0px;font-weight:400">PEDIDO</h1>
-        <hr style="border:1px solid black"/>
-        <h2 style="text-align:center;font-size:35px;background-color:#ccc;margin:0px;font-weight:400">Torres Paulista</h2>
-        <span style="text-align:center;font-size:15px;background-color:#ccc;margin:0px">CNPJ 55.555.888/0001-99</span>
-        <span style="text-align:center;font-size:15px;background-color:#ccc;margin:0px">Fone: 11-2666-8899 São Paulo-SP
-        <hr/>
-        <h2 style="font-weight:400;font-size:40px; padding:0px;height:40px">${mesa}</h2>
-        <hr/>
-        <h4>Atendente: ${atendente}</h4>
-        <hr/>
-        <h3>Cliente:${cliente}</h3>
-        <div>
-        <h3>Data:${data}</h3>
-        <h3>Hora:${hora}</h3>
+        <h1 style="text-align:center;font-size:70px;background-color:#ccc;margin:0px;font-weight:400;padding:15px 0">PEDIDO</h1>
+        
+        <div style="background-color:#ccc;display:flex;flex-direction:column;height:100px;padding:15px 0;border-top:3px solid black">
+        <h2 style="text-align:center;font-size:40px;margin:0px;font-weight:400">Torres Paulista</h2>
+        <span style="text-align:center;font-size:20px;margin:5px;height:20px">CNPJ 55.555.888/0001-99</span>
+        <span style="text-align:center;font-size:20px;margin:5px; height:20px">Fone: 11-2666-8899 São Paulo-SP
         </div>
-        <div >
-         ${pedido.map((item)=>('<div style="display:flex;justify-content:space-between;height:15px"> <span>'+item.qtd+ ' x '+item.produto+'</span>'+'<span>'+(item.qtd*item.precoUnit).toFixed(2).replace('.',',')+'</span> </div>' ))}
+        
+        <h2 style="font-weight:400;font-size:55px;text-align:center; background-color:#fff;margin:0px; padding:30px 0px;height:50px;border-top:3px solid black">${mesa}</h2>
+        
+        <h4 style="display:flex;justify-content:center;font-size:25px;height:35px;font-weight:400;margin:0;padding:8px 0;border-top:3px solid black;border-bottom:3px solid black">Atendente: ${atendente}</h4>
+        
+        <h3 style="font-size:30px;margin:13px;font-weight:400;align-items:center;height:30px">Cliente:${cliente}</h3>
+        <div style="display:flex;flex-direction:row;margin:0 10px;font-weight:400; align-items:space-between;justify-content:center">
+        <h3 style="flex:1;font-size:30px;font-weight:400;margin:0">Data:${data}</h3>
+        <h3 style="font-size:30px;font-weight:400;margin:0">Hora:${hora}</h3>
+        </div>
+        <div style="margin:35px 0">
+         ${pedido.map((item)=>('<div style="display:flex;justify-content:space-between;height:35px;font-size:35px"> <span>'+item.qtd+ ' x '+item.produto+'</span>'+'<span>'+(item.qtd*item.precoUnit).toFixed(2).replace('.',',')+'</span> </div>' ))}
         </div>
        <hr/>
-       <div  style="display:flex;justify-content:space-between;height:15px;padding:15px 30px">
-       <span> Din ${totalNota.toFixed(2).replace('.',',')}</span>
-       <span> PIX ${totalNota.toFixed(2).replace('.',',')} </span>
-       <span> Déb ${eval(totalNota*1.05).toFixed(2).replace('.',',')}</span>
-       <span> Créd ${eval(totalNota*1.15).toFixed(2).replace('.',',')}</span>
+       <div  style="display:flex;justify-content:space-between;background-color:#ccc;height:35px;padding:15px 30px;border-top:3px solid black;border-bottom:3px solid black">
+       <span style="font-size:30px"> Din ${totalNota.toFixed(2).replace('.',',')}</span>
+       <span style="font-size:30px"> PIX ${totalNota.toFixed(2).replace('.',',')} </span>
+       <span style="font-size:30px"> Déb ${eval(totalNota*1.05).toFixed(2).replace('.',',')}</span>
+       <span style="font-size:30px"> Créd ${eval(totalNota*1.15).toFixed(2).replace('.',',')}</span>
+        </div>
+        <div style="background-color:#ccc;display:flex;flex-direction:row;padding:0 10%; margin:0">
+        <h3 style="flex:1;font-size:30px;font-weight:400"> Qtde de Itens</h3>
+        <h3 style="font-size:30px;font-weight:400"> ${totalItens<10?'0'+totalItens:totalItens}</h3>
+        </div>
+        <div style="background-color:#ccc;display:flex;flex-direction:row;padding:0 10%; margin:0">
+        <h3 style="flex:1;font-size:30px;font-weight:400;margin:0"> TOTAL(+10%)</h3>
+        <h3 style="font-size:30px;font-weight:400;margin:0;padding-bottom:15px"> R$ ${eval(totalItens*1.1).toFixed(2).replace('.',',')}</h3>
         </div>
     </body>
    
